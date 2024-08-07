b77ed8ea6fc45be1dbce7533384b2e10553238f6
Faris Jaafar <farisgear@gmail.com>
2024-08-07 16:22:42 +0800

    Enumerable.Emptyand Array.Empty benchmark

---
+++ src/PerformanceSeries/EmptyCollectionBenchmark/BenchmarkEmptyCollection.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace EmptyCollectionBenchmark
+{
+    [MemoryDiagnoser]
+    public class BenchmarkEmptyCollection
+    {
+        private IEnumerable<Student> _students;
+        [Benchmark(Baseline = true)]
+        public void ReturnNew()
+        {
+            _students = new List<Student>();
+        }
+
+        [Benchmark]
+        public void ReturnEmtpy()
+        {
+            _students = Enumerable.Empty<Student>();
+        }
+
+    }
+}
---
+++ src/PerformanceSeries/EmptyCollectionBenchmark/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace EmptyCollectionBenchmark
+{
+    public sealed class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
