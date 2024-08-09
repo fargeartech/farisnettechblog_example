cb8c3e7a6d0881c51f67bffe0a79694b991de771
Faris Jaafar <farisgear@gmail.com>
2024-08-09 10:42:50 +0800

    add equal string benchmark

---
+++ src/PerformanceSeries/StringEqualBenchDemo/Benchy.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace StringEqualBenchDemo
+{
+    [MemoryDiagnoser]
+    [SimpleJob(runtimeMoniker: RuntimeMoniker.Net80)]
+    public class Benchy
+    {
+        private readonly string compare1 = "my_compare";
+        private readonly string compare2 = "my_compare";
+
+        [Benchmark(Baseline = true)]
+        public bool DirectEqual()
+        {
+            return compare1 == compare2;
+        }
+
+        [Benchmark]
+        public bool ToLower()
+        {
+            return compare1.ToLower() == compare2.ToLower();
+        }
+
+        [Benchmark]
+        public bool CompareEqual()
+        {
+            return string.Equals(compare1, compare2);
+        }
+
+        [Benchmark]
+        public bool CompareEqualWithIgnoreCase()
+        {
+            return string.Equals(compare1, compare2, StringComparison.OrdinalIgnoreCase);
+        }
+
+    }
+}
