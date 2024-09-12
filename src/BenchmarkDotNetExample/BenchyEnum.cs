ebbea6a1e2fbb376dba61c029a9d86cf6ef460d6
Faris Jaafar <farisgear@gmail.com>
2024-09-12 15:21:25 +0800

    using StyleConfig for custom output

--- src/BenchmarkDotNetExample/BenchyEnum.cs
+++ src/BenchmarkDotNetExample/BenchyEnum.cs
@@ -1,8 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
 
 namespace BenchmarkDotNetExample
 {
     [MemoryDiagnoser]
+    [Config(typeof(StyleConfig))]
     public class BenchyEnum
     {
         [Benchmark(Baseline = true)]
@@ -15,6 +19,12 @@ namespace BenchmarkDotNetExample
         public string FastEnum()
         {
             return Enums.GetFastEnum(Enums.UserType.Admin);
+        }
+
+        private class StyleConfig : ManualConfig
+        {
+            public StyleConfig()
+                => SummaryStyle = SummaryStyle.Default.WithRatioStyle(RatioStyle.Percentage);
         }
     }
 }
